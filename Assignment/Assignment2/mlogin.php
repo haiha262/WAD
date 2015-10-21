a13c151e11d40b5942f77bf4065d050390293c32
Michael <haiha262@gmail.com>
2015-10-21 19:44:47 +1100

    - run on mercury

--- Assignment/Assignment2/mlogin.php
+++ Assignment/Assignment2/mlogin.php
@@ -5,7 +5,7 @@ unset($_SESSION["admin_name"]);//?not work
 
 header('Content-Type: text/xml, text/plan');
 //txtfile
-$filePath = "./home/students/accounts/s4959353/cos80021/www/data/managers.txt";
+$filePath = "../../data/managers.txt";
 if(isset($_POST["admin_name"]) && isset($_POST["password"]))
 {
 	$admin_name = trim($_POST["admin_name"]);
